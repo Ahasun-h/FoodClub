aa93c29524f80f1b572e2ec2576b480363b1d558
Ahasun Habib <33959275+Ahasun-h@users.noreply.github.com>
2023-02-12 21:30:28 +0600

    First Commit

---
+++ FoodClub/FoodClubApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodClubApp.swift
+//  FoodClub
+//
+//  Created by Ahasun on 1/26/23.
+//
+
+import SwiftUI
+
+@main
+struct FoodClubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
